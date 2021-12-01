10b49db3f6c81dbdef9449007e4d9f8b481d1e1b
unknown <mehediph2021@gmail.com>
2021-12-01 03:08:51 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,72 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
-import Slider from './componetn/Slidre/Slider';
+import Text from './text/Text';
+import Footer from './Footer/Footer';
 
 
 function App() {
   return (
-    <div className="App mt-5 bg-secondary">
-      <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      centeredSlides
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={swiper => console.log(swiper)}
-    >
-      <SwiperSlide>
-          <div class="card mt-5 mb-5">
-      <div class="card-body">
-        <h1> hello Mehedi </h1>
-        <p class="card-text">Some quick example text to build on the card title and make
-        up the bulk of the card's content.</p>
-      </div>
-    </div>
-    </SwiperSlide>
-      <SwiperSlide>
-          <div class="card mt-5 mb-5">
-      <div class="card-body">
-        <h1> hello Mehedi </h1>
-        <p class="card-text">Some quick example text to build on the card title and make
-        up the bulk of the card's content.</p>
-      </div>
-    </div>
-    </SwiperSlide>
-      <SwiperSlide>
-          <div class="card mt-5 mb-5">
-      <div class="card-body">
-        <h1> hello Mehedi </h1>
-        <p class="card-text">Some quick example text to build on the card title and make
-        up the bulk of the card's content.</p>
-      </div>
-    </div>
-    </SwiperSlide>
-      <SwiperSlide>
-          <div class="card mt-5 mb-5">
-      <div class="card-body">
-        <h1> hello Mehedi </h1>
-        <p class="card-text">Some quick example text to build on the card title and make
-        up the bulk of the card's content.</p>
-      </div>
-    </div>
-    </SwiperSlide>
-      <SwiperSlide>
-          <div class="card mt-5 mb-5">
-      <div class="card-body">
-        <h1> hello Mehedi </h1>
-        <p class="card-text">Some quick example text to build on the card title and make
-        up the bulk of the card's content.</p>
-      </div>
-    </div>
-    </SwiperSlide>
+  <div >
+    <Footer></Footer>
 
-      
-    </Swiper>
-     
-     <Slider></Slider>
-    </div>
+
+  </div>
+ 
   );
 }
 
